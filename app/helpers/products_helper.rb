b1f917f4ff58bbe7432125220eba3c3e8b626edc
Yos Riady <yosriady@gmail.com>
2014-05-21 15:34:26 +0800

    Add Stock view helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,5 +4,12 @@ def cents_price(price)
     "#{(price*100).floor} cents"
 end
 
+def print_stock(stock)
+    if stock > 0
+        content_tag(:span, "In Stock (#{stock})", class: "in_stock")
+    else
+        content_tag(:span, "Out of Stock", class: "out_stock")
+    end
+end
 
 end
