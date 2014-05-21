876f66748b20e27bf495d39bf8054a11400478c0
Yos Riady <yosriady@gmail.com>
2014-05-21 15:13:37 +0800

    Add Stock to Product, remove debugger from Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521055947) do
+ActiveRecord::Schema.define(version: 20140521070049) do
 
   create_table "products", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140521055947) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "stock",                               default: 0
   end
 
 end
