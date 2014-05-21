e43aaa393e1f93f04b5a5f331638ead50d6e902a
Yos Riady <yosriady@gmail.com>
2014-05-21 18:23:21 +0800

    Calculate order subtotal + total

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ActiveRecord::Base
     has_many :order_items
 
+    def total
+        sum = 0
+        order_items.each {|item| sum += item.subtotal}
+        return sum
+    end
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,11 @@
 class OrderItem < ActiveRecord::Base
+    validates_presence_of :product_id, :order_id
+
     belongs_to :order
+    belongs_to :product
+
+    def subtotal
+        quantity * self.product.price
+    end
+
 end
