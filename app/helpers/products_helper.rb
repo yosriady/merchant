44b8672bbba583655bfc1ff285bdad42fde37371
Yos Riady <yosriady@gmail.com>
2014-05-22 16:19:11 +0800

    Order stock validation

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,10 @@
 module ProductsHelper
 
-def print_stock(stock)
+def print_stock(stock, requested = 1)
     if stock > 0
         content_tag(:span, "In Stock (#{stock})", class: "in_stock")
+    elsif stock < requested
+        content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
     else
         content_tag(:span, "Out of Stock", class: "out_stock")
     end
