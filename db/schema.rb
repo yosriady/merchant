1acb4b1ed0271abd4bf56281880d11021e21e5b9
Yos Riady <yosriady@gmail.com>
2014-05-22 14:52:56 +0800

    Migrate, add 0 default to OrderItem quantity

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521080246) do
+ActiveRecord::Schema.define(version: 20140522023724) do
 
   create_table "order_items", force: true do |t|
     t.integer  "product_id"
     t.integer  "order_id"
-    t.integer  "quantity"
+    t.integer  "quantity",   default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
